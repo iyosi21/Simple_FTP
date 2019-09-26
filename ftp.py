205bb185f118f65782835cfee945684932784b99
t_kasajima <hi_tech.iyosi@shigokiru.com>
2019-09-26 12:32:05 +0900

    IPアドレスとパス取得

--- ftp.py
+++ ftp.py
@@ -2,15 +2,24 @@
 import pyftpdlib.authorizers
 import pyftpdlib.handlers
 import pyftpdlib.servers
+import socket
+import os.path
+
+# ローカルホストのIPアドレスを取得
+host = socket.gethostname
+ip = socket.gethostbyname(host)
+
+# 実行ファイルのパスを取得
+cur_path = os.path.dirname(__file__)
 
 # 認証ユーザーを作る
 authorizer = pyftpdlib.authorizers.DummyAuthorizer()
-authorizer.add_user('user', 'password', 'C:\\testtest', perm='elradfmw')
+authorizer.add_user('user', 'password', cur_path, perm='elradfmw')
 
 # 個々の接続を管理するハンドラーを作る
 handler = pyftpdlib.handlers.FTPHandler
 handler.authorizer = authorizer
 
 # FTPサーバーを立ち上げる
-server = pyftpdlib.servers.FTPServer(("XXX.XXX.XXX.XXX", 21), handler)
+server = pyftpdlib.servers.FTPServer((ip, 21), handler)
 server.serve_forever()
